c8b3e3ecf4c29e6acaebbaef1706039b1dc8af8c
Stephen Makilan <stephensmakilan@gmail.com>
2021-09-11 11:59:43 +0800

    ignore .DS_Store

--- Section3/01-starting-setup/src/components/Expenses/Expenses.js
+++ Section3/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import "./Expenses.css";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.data[0].title}
         amount={props.data[0].amount}
@@ -24,7 +25,7 @@ function Expenses(props) {
         amount={props.data[3].amount}
         date={props.data[3].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
